5dfc3947bdcf5b696d36c1360cd1dc084c3ed4af
Mohammad <shushinasabm@gmail.com>
2022-03-29 20:52:36 +0430

    Make capslock and shift keys functional

--- script.js
+++ script.js
@@ -1,61 +1,62 @@
-const icon = document.getElementById("icon");
-const bodyTag = document.getElementsByTagName("body")[0];
-const keyDark = document.getElementsByTagName("span")
-const detailTag = document.getElementById("detail")
-let text = ""
+let shift_keys = document.querySelectorAll('.shift')
+for (let shift_key of shift_keys) {
+    shift_key.addEventListener('click', function (event) {
+        let capslock_keys = document.querySelectorAll('.capsLock')
 
-console.log(icon)
+        for (let capslock of capslock_keys)
+            capslock.classList.toggle('capsLock-on')
 
-function changeKeyColor() {
-    if (bodyTag.classList.item(0) === "body-light") {
-        for (const key of keyDark) {
-            key.classList.replace("key", "key-dark")
+        let capslock = document.querySelector('.capsLock')
+        if (capslock.classList.contains('capsLock-on')) {
+            let xs_keys = document.querySelectorAll('.key-xs')
+            for (let xs_key of xs_keys) {
+                if (!xs_key.classList.contains('key-fn')) {
+                    if (xs_key.classList.contains('none-letter'))
+                        xs_key.innerText = xs_key.getAttribute('shift-on')
+
+                    if (!xs_key.classList.contains('none-letter'))
+                        xs_key.innerText = xs_key.innerText.toUpperCase()
+                }
+            }
         }
-    } else {
-        for (const key of keyDark) {
-            key.classList.replace("key-dark", "key")
+
+        if (!capslock.classList.contains('capsLock-on')) {
+            let xs_keys = document.querySelectorAll('.key-xs')
+            for (let xs_key of xs_keys) {
+                if (!xs_key.classList.contains('key-fn')) {
+                    if (xs_key.classList.contains('none-letter'))
+                        xs_key.innerText = xs_key.getAttribute('shift-off')
+
+                    if (!xs_key.classList.contains('none-letter'))
+                        xs_key.innerText = xs_key.innerText.toLowerCase()
+                }
+            }
+        }
+    })
+}
+
+let capslock_key = document.querySelector('.caps-lock')
+capslock_key.addEventListener('click', function (event) {
+    let capslock = this.querySelector('.capsLock')
+    capslock.classList.toggle('capsLock-on')
+
+    if (capslock.classList.contains('capsLock-on')) {
+        let xs_keys = document.querySelectorAll('.key-xs')
+        for (let xs_key of xs_keys) {
+            if (!xs_key.classList.contains('key-fn')) {
+                if (!xs_key.classList.contains('none-letter'))
+                    xs_key.innerText = xs_key.innerText.toUpperCase()
+            }
         }
     }
-}
 
-function changeBodyColor() {
-
-    if (bodyTag.classList.item(0) === 'body-light') {
-        bodyTag.classList.replace("body-light", "body-dark")
-        icon.style.color = "white"
-    } else {
-        bodyTag.classList.replace("body-dark", "body-light")
-        icon.style.color = "black"
+    if (!capslock.classList.contains('capsLock-on')) {
+        let xs_keys = document.querySelectorAll('.key-xs')
+        for (let xs_key of xs_keys) {
+            if (!xs_key.classList.contains('key-fn')) {
+                if (!xs_key.classList.contains('none-letter'))
+                    xs_key.innerText = xs_key.innerText.toLowerCase()
+            }
+        }
     }
-}
-
-function clickEvent() {
-    changeKeyColor()
-    changeBodyColor()
-}
-
-icon.addEventListener("click", clickEvent);
-for (const key of keyDark) {
-    key.addEventListener("click", typeEvent)
-}
-
-console.log(keyDark)
-
-function typeEvent(event) {
-    if (event.target.innerText === "Delete") {
-        text = text.slice(0, -1)
-
-    } else if (event.target.innerText === "Enter") {
-        text += "\n"
-
-    } else if (event.target.innerText === "Tab") {
-        text += "&nbsp;"
-        text += "&nbsp;"
-        text += "&nbsp;"
-        text += "&nbsp;"
-
-    } else {
-        text += event.target.innerText
-    }
-    detailTag.innerHTML = text
-}
+})
