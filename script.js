e7c5b63da74fc42279fa2c9c4836e755bb879174
shushinasabm <98877361+shushinasabm@users.noreply.github.com>
2022-04-08 18:02:33 +0430

    the end

--- script.js
+++ script.js
@@ -10,33 +10,38 @@ let shift_keys = document.querySelectorAll('.shift')
 for (let shift_key of shift_keys) {
     shift_key.addEventListener('click', function (event) {
         let capslock_keys = document.querySelectorAll('.capsLock')
-
-        for (let capslock of capslock_keys)
-            capslock.classList.toggle('capsLock-on')
-
-        let capslock = document.querySelector('.capsLock')
-        if (capslock.classList.contains('capsLock-on')) {
-            let xs_keys = document.querySelectorAll('.key-xs')
-            for (let xs_key of xs_keys) {
-                if (!xs_key.classList.contains('key-fn')) {
-                    if (xs_key.classList.contains('none-letter'))
-                        xs_key.innerText = xs_key.getAttribute('shift-on')
-
-                    if (!xs_key.classList.contains('none-letter'))
-                        xs_key.innerText = xs_key.innerText.toUpperCase()
-                }
+        let shift_caps = null
+        for (let capslock of capslock_keys) {
+            if (capslock.parentElement.classList.contains('shift')) {
+                capslock.classList.toggle('capsLock-on')
+                shift_caps = capslock
             }
         }
 
-        if (!capslock.classList.contains('capsLock-on')) {
-            let xs_keys = document.querySelectorAll('.key-xs')
-            for (let xs_key of xs_keys) {
-                if (!xs_key.classList.contains('key-fn')) {
-                    if (xs_key.classList.contains('none-letter'))
-                        xs_key.innerText = xs_key.getAttribute('shift-off')
+        if (shift_caps !== null) {
+            if (shift_caps.classList.contains('capsLock-on')) {
+                let xs_keys = document.querySelectorAll('.key-xs')
+                for (let xs_key of xs_keys) {
+                    if (!xs_key.classList.contains('key-fn')) {
+                        if (xs_key.classList.contains('none-letter'))
+                            xs_key.innerText = xs_key.getAttribute('shift-on')
 
-                    if (!xs_key.classList.contains('none-letter'))
-                        xs_key.innerText = xs_key.innerText.toLowerCase()
+                        if (!xs_key.classList.contains('none-letter'))
+                            xs_key.innerText = xs_key.innerText.toUpperCase()
+                    }
+                }
+            }
+
+            if (!shift_caps.classList.contains('capsLock-on')) {
+                let xs_keys = document.querySelectorAll('.key-xs')
+                for (let xs_key of xs_keys) {
+                    if (!xs_key.classList.contains('key-fn')) {
+                        if (xs_key.classList.contains('none-letter'))
+                            xs_key.innerText = xs_key.getAttribute('shift-off')
+
+                        if (!xs_key.classList.contains('none-letter'))
+                            xs_key.innerText = xs_key.innerText.toLowerCase()
+                    }
                 }
             }
         }
@@ -99,7 +104,7 @@ delete_key.addEventListener('click', function (event) {
         detail.value = detail.value.slice(0, -1)
 })
 let enter_key = document.querySelector('.enter')
-enter_key.addEventListener('click',function () {
+enter_key.addEventListener('click', function () {
     let detail = document.getElementById('detail')
     detail.value += '\n'
 })
